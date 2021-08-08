fac3a7f564c3aeba66286f59289e35716774a34b
livmari <liv.alterskjaer@icloud.com>
2021-08-09 00:02:54 +0200

    Grid layout added to footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,18 +1,31 @@
 import type { FC } from 'react'
+import Link from 'next/link'
 import { Facebook, Instagram, Linkedin } from 'react-feather'
+
+import { Button } from '@components/index'
 
 import styles from './Footer.module.sass'
 
 const Footer: FC = () => {
   return (
     <footer className={styles.appFooter}>
-      <h2>
-        Vivation
-        <br />
-        Skandinavien
-      </h2>
+      <div className={styles.brandContainer}>
+        <Link href={'/'}>
+          <a className={styles.brand}>
+            <h2>
+              Vivation
+              <br />
+              Skandinavien
+            </h2>
+          </a>
+        </Link>
+      </div>
 
-      <ul>
+      <div className={styles.buttonContainer}>
+        <Button label={'Boka en konsultation'} />
+      </div>
+
+      <ul className={styles.contactInfoList}>
         <li>info@vivation.se</li>
 
         <li>+4676-626 50 50</li>
@@ -20,7 +33,7 @@ const Footer: FC = () => {
         <li>Göteborg, Sverige</li>
       </ul>
 
-      <ul>
+      <ul className={styles.socialMediaList}>
         <li>
           <Facebook />
         </li>
