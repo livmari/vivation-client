6b181c52a70546d5a7b91d959d7a374dd9f8d3ca
livmari <liv.alterskjaer@icloud.com>
2021-08-08 21:53:31 +0200

    Info renamed to callout

--- components/pages/home/index.ts
+++ components/pages/home/index.ts
@@ -1,6 +1,6 @@
 export { default as SectionHero } from './SectionHero/SectionHero'
 
-export { default as SectionInfo } from './SectionInfo/SectionInfo'
+export { default as SectionCallout } from './SectionCallout/SectionCallout'
 export type { SectionHeroProps } from './SectionHero/SectionHero.props'
 
 export { default as SectionTestimonials } from './SectionTestimonials/SectionTestimonials'
