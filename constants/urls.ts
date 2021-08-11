51c222fb222f0f7c260c824d2ec2e94e56946f48
livmari <liv.alterskjaer@icloud.com>
2021-08-11 21:15:39 +0200

    Forgot to add prod cms

--- constants/urls.ts
+++ constants/urls.ts
@@ -1,2 +1,4 @@
 export const STRAPI_BASE_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:1337' : '' // TODO: add prod env url once deployed to heroku
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:1337'
+    : 'https://www.vivation-cms.herokuapp.com'
